363cfd1895c9ea67960f59775002fdfadbd5bcd2
Rohan Mars <code@rohanmars.com>
2024-03-08 16:50:34 -0800

    cekit build - full,k8s,okd,gke,aws (#3)

--- build.sh
+++ build.sh
@@ -6,3 +6,7 @@ dart pub get
 dart pub run flutter_oss_licenses:generate.dart -o oss_licenses.json --json
 dart run build_runner build
 dart compile exe bin/tiecd.dart -o bin/tiecd
+mkdir -p images/modules/base/build
+cp bin/tiecd images/modules/base/build
+cp oss_licenses.json images/modules/base/build
+cp LICENSE images/modules/base/build
---
+++ scripts/build-image.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# prime resources into base module
+mkdir images/modules/base/build
+cp bin/tiecd images/modules/base/build/.
+cp LICENSE images/modules/base/build/.
+cp oss_licenses.json images/modules/base/build/.
+
+while read tag; do
+  if [ "$1" == "full" ]; then
+    tagargs+=( --tag="$tag" )
+  else
+    tagargs+=( --tag="$tag-$1" )
+  fi
+done <tags.txt
+
+cekit --descriptor images/$1.yaml build docker "${tagargs[@]}"
+
+while read tag; do
+  if [ "$1" == "full" ]; then
+    echo "docker push $tag"
+    docker push $tag
+  else
+    echo "docker push $tag-$1"
+    docker push $tag-$1
+  fi
+done <tags.txt
+
